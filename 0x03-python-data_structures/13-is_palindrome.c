32a779a39267c35595ad31182099980cd9f8d2c0
SimonHL13 <usiagwusimon@gmail.com>
2021-09-13 05:34:41 -0700

    a function in C that checks if a singly linked list is a palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,52 +1,31 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * is_palindrome - checks is linked list is a palindrome
- * @head: Double pointer to head
- * Return: 1 if true else 0
+ * palindrom -  recursive palind or not
+ * @head: head list
+ * Return: 0 if it is not a palindrome
+ * 1 if it is a palindrome
  */
 int is_palindrome(listint_t **head)
 {
-listint_t *c1;
-unsigned int len = 0, i = 0;
-unsigned int *arr;
-
-if (head == NULL)
-return (0);
-if (*head == NULL)
+if (head == NULL || *head == NULL)
 return (1);
-c1 = *head;
-while (c1 != NULL)
-{
-c1 = c1->next;
-len++;
-}
-if (len == 1)
-return (1);
-arr = malloc(sizeof(int) * len)
-if (arr == NULL)
-{
-free(arr);
-return (0);
-}
-c1 = *head;
-for (i = 0; c1 != NULL; i++)
-{
-arr[i] = c1->
-c1 = c1->next;
-}
-len = len - 1;
-for (i = 0; i <= len / 2; i++)
-{
-if (arr[i] != arr[len - i])
-{
-free(arr);
-return (0);
-}
-}
-free(arr);
-return (1);
+return (aux_palind(head, *head));
 }
 
+/**
+ * aux_palind - funct to know if is palindrome
+ * @head: head list
+ * @end: end list
+ */
+int aux_palind(listint_t **head, listint_t *end)
+{
+if (end == NULL)
+return (1);
+if (aux_palind(head, end->next) && (*head)->n == end->n)
+{
+*head = (*head)->next;
+return (1);
+}
+return (0);
+}
